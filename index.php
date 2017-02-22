15784990e980ef163f7bd96d7694a4104e4d2024
Webleyson <webleyson@users.noreply.github.com>
2017-02-22 09:54:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ include_once 'config/database.php';
 include_once 'objects/product.php';
 include_once 'objects/category.php';
 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
 ?>
 
 // main content of web page must be here!
