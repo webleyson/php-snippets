4a9d54dbed3875cbd01730270a57068b41219768
Webleyson <webleyson@users.noreply.github.com>
2017-02-22 09:45:21 +0000

    index with db include

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 include_once 'header.php';
+include_once 'config/database.php';
+include_once 'objects/product.php';
+include_once 'objects/category.php';
+
 ?>
+
 // main content of web page must be here!
 <?php
 include_once 'footer.php';
